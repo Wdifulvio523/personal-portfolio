92266e34025451cd337bf14a352bb6b0330a3562
Lambda_School_Loaner_46 <lambda_school_loaner_46@Lambda-School-Loaner-46s-MacBook-Pro.local>
2018-12-20 17:52:36 -0500

    initial commit -wd

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,50 +1,47 @@
-const contentful = require('contentful');
-const manifestConfig = require('./manifest-config');
-require('dotenv').config();
-
-const { ACCESS_TOKEN, SPACE_ID } = process.env;
-
-const client = contentful.createClient({
-  space: SPACE_ID,
-  accessToken: ACCESS_TOKEN,
-});
-
-module.exports = client.getEntries().then(entries => {
-  const aboutEntry = entries.items.find(
-    entry => entry.sys.contentType.sys.id === 'about',
-  );
-
-  const about = aboutEntry.fields;
-
-  return {
-    plugins: [
-      'gatsby-plugin-react-helmet',
-      {
-        resolve: 'gatsby-plugin-manifest',
-        options: manifestConfig,
-      },
-      'gatsby-plugin-styled-components',
-      {
-        resolve: `gatsby-plugin-google-fonts`,
-        options: {
-          fonts: [`cabin`, `Open Sans`],
-        },
-      },
-      {
-        resolve: `gatsby-source-contentful`,
-        options: {
-          spaceId: SPACE_ID,
-          accessToken: ACCESS_TOKEN,
-        },
-      },
-      {
-        resolve: `gatsby-source-medium`,
-        options: {
-          username: about.mediumUser,
-        },
-      },
-      'gatsby-transformer-remark',
-      'gatsby-plugin-offline',
-    ],
-  };
-});
+module.exports = {
+  siteMetadata: {
+    title: "Gatsby Starter - Ceevee",
+    author: "Aman Mittal",
+    description: "A Gatsby.js Starter based on Ceevee by Styleshout"
+  },
+  pathPrefix: "/",
+  plugins: [
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography.js`
+      }
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/assets/images`
+      }
+    },
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [`librebaskerville`, `open sans`]
+      }
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 630
+            }
+          },
+          "gatsby-remark-copy-linked-files"
+        ]
+      }
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sass`
+  ]
+};
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,81 +1,35 @@
-import React from 'react';
-import styled from 'styled-components';
-import { Text, Flex, Box } from 'rebass';
-import Fade from 'react-reveal/Fade';
-import PropTypes from 'prop-types';
-import ContentfulLogo from './Logo/Contenful.svg';
-import GatsbyLogo from './Logo/Gatsby.svg';
-
-const FooterContainer = styled.footer`
-  padding: 1em;
-  background: ${props => props.theme.colors.primaryDark};
-  color: ${props => props.theme.colors.background};
-  display: flex;
-  flex: 0 1 auto;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-`;
-
-const RenponsiveLogo = styled.img`
-  width: 100px;
-  height: 25px;
-
-  @media (min-width: 400px) {
-    width: 150px;
-    height: 35px;
-  }
-`;
-
-const Logo = ({ url, logo, alt = '' }) => (
-  <Box>
-    <a href={url} rel="noopener noreferrer" target="_blank">
-      <RenponsiveLogo src={logo} alt={alt} />
-    </a>
-  </Box>
-);
-
-Logo.propTypes = {
-  url: PropTypes.string.isRequired,
-  logo: PropTypes.string.isRequired,
-  alt: PropTypes.string,
-};
+import React from "react";
+import SocialLinks from "./SocialLinks";
+import { FaChevronCircleUp } from "react-icons/lib/fa";
 
 const Footer = () => (
-  <FooterContainer>
-    <Fade bottom>
-      <span>
-        <Text
-          mb={2}
-          pb={1}
-          css={{
-            textTransform: 'uppercase',
-            borderBottom: 'white 3px solid',
-            display: 'table',
-          }}
-        >
-          Powered By
-        </Text>
-      </span>
-      <Flex justifyContent="center" alignItems="center">
-        <Logo
-          url="https://www.contentful.com/"
-          logo={ContentfulLogo}
-          alt="Powered by Contentful"
-        />
-        <Text m={2} fontSize={4}>
-          <span role="img" aria-label="heart">
-            ❤️
-          </span>
-        </Text>
-        <Logo
-          url="https://www.gatsbyjs.org/"
-          logo={GatsbyLogo}
-          alt="Gatsby Logo"
-        />
-      </Flex>
-    </Fade>
-  </FooterContainer>
+  <footer id="footer">
+    <div className="row">
+      <div className="twelve columns">
+        <SocialLinks />
+
+        <ul className="copyright">
+          <li>&copy; Copyright 2018 CeeVee</li>
+          <li>
+            Design by{" "}
+            <a title="Styleshout" href="http://www.styleshout.com/">
+              Styleshout
+            </a>
+          </li>
+          <li>
+            Developed in React ⚛️ by{" "}
+            <a href="http://www.amanhimself.me">Aman Mittal</a>
+          </li>
+        </ul>
+      </div>
+
+      <div id="go-top">
+        <a className="smoothscroll" title="Back to Top" href="#home">
+          <FaChevronCircleUp />
+        </a>
+      </div>
+    </div>
+  </footer>
 );
 
 export default Footer;
